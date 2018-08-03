ff42addc27d3e93285d8d35a3a75dec86055e6e8
Victor Freitas <victorf.santos@outlook.com>
2018-08-03 11:48:02 -0300

    criamos a ui e adicionamos a funcao da boas vindas

---
+++ ui.rb
@@ -0,0 +1,11 @@
+def da_boas_vindas
+	puts "=============="
+	puts "== FogeFoge =="
+	puts "==============\n\n"
+
+	puts "Qual é seu nome?"
+	nome = gets.strip
+	puts "Começaremos o jogo para você, #{nome}!"
+	nome
+end
+
